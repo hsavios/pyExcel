551bd6700df16f0ea1c8d190d996519ecd4782ea
hsavios <suporte@heliosavio.com>
2023-04-19 13:27:12 -0400

    Refatorado para somar os pesos

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+import os
 import customtkinter as ctk
-from openpyxl import Workbook
+from openpyxl import Workbook, load_workbook
 import pyperclip
 import time
 
@@ -8,7 +9,7 @@ class Application(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.title("Cadastro de dados")
-        self.geometry("300x400")
+        self.geometry("300x250")
 
         self.date_label = ctk.CTkLabel(self, text="Data:")
         self.date_label.pack()
@@ -38,9 +39,23 @@ class Application(ctk.CTk):
             self, text="Salvar", command=self.save_data)
         self.save_button.pack()
 
-        self.wb = Workbook()
-        self.ws = self.wb.active
-        self.ws.append(["Data", "Placa", "Estado", "Peso"])
+        self.total_weight_label = ctk.CTkLabel(self, text="Total de peso: 0")
+        self.total_weight_label.pack()
+
+        self.file_name = "dados.xlsx"
+        if os.path.isfile(self.file_name):
+            self.wb = load_workbook(self.file_name)
+            self.ws = self.wb.active
+            self.update_total_weight()
+        else:
+            self.wb = Workbook()
+            self.ws = self.wb.active
+            self.ws.append(["Data", "Placa", "Estado", "Peso"])
+            self.total_weight = 0
+
+    def update_total_weight(self):
+        total_weight = sum([float(cell.value) for cell in self.ws['D'][1:]])
+        self.total_weight_label.configure(text=f"Total de peso: {total_weight}")
 
     def save_data(self):
         date = self.date_entry.get()
@@ -49,13 +64,15 @@ class Application(ctk.CTk):
         weight = self.weight_entry.get()
 
         self.ws.append([date, plate, state, weight])
-        self.wb.save("dados.xlsx")
+        self.wb.save(self.file_name)
 
         pyperclip.copy(f"{plate}")
         time.sleep(3)
         pyperclip.copy(f"{state}")
         time.sleep(3)
         pyperclip.copy(f"{weight}")
+
+        self.update_total_weight()
 
 
 app = Application()
